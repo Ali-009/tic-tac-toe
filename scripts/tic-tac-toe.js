6167bb8e108bb6e38934be259bd771cb84466d2d
AliTinai <ali-009@windowslive.com>
2021-07-27 15:16:05 +0300

    Set the basic structure of the page

---
+++ scripts/tic-tac-toe.js
@@ -0,0 +1,52 @@
+//Player Factory Function
+function Players(name){
+  let score = 0;
+  return {name, score};
+}
+
+let gameBoard = (function(){
+  let gameBoardArray = [];
+
+  function createBoard(){
+    let wrapper = document.querySelector('#wrapper');
+    let board = document.createElement('div');
+    wrapper.appendChild(board);
+    board.setAttribute('id','board');
+
+    //creating cells within the board
+    for(let i = 0; i < 9; i++){
+      let boardCell = document.createElement('div');
+      boardCell.classList.add('board-cell');
+      board.appendChild(boardCell);
+
+      if(i === 0 || i === 3 || i === 6){
+        boardCell.style.borderLeft = 'none';
+      }
+
+      if(i === 6 || i === 7 || i === 8){
+        boardCell.style.borderBottom = 'none';
+      }
+    }
+  }
+
+  return{gameBoardArray, createBoard};
+})();
+
+
+//Initialize Game
+function startGame(){
+
+  let twoPlayerButton = document.querySelector('#two-player-button');
+
+  twoPlayerButton.addEventListener('click', initGame);
+
+}
+
+function initGame(e){
+    player1 = Players(prompt('Enter the name of the first player'));
+    player2 = Players(prompt('Enter the name of the second player'));
+
+    gameBoard.createBoard();
+}
+
+startGame();

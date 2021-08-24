2d95f1ea5619370eef9729efd86e11edf86152c4
AliTinai <ali-009@windowslive.com>
2021-08-24 11:16:27 +0300

    Added the basic modules and a player factory function

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,1 +1,60 @@
 
+//Modules
+
+let gameBoard = (function(){
+  let state = [];
+  return {state};
+})();
+
+let gameFlow = (function(){
+  function startGame(){
+
+  }
+
+  function storeValue(){
+
+  }
+
+  function checkWinner(){
+
+  }
+
+  return {startGame, storeValue, checkWinner};
+})();
+
+let displayController = (function(){
+
+  function displayBoard(){
+
+  }
+
+  function addEventListeners(){
+
+  }
+
+  function endMatch(){
+
+  }
+
+  function removeBoard(){
+
+  }
+
+  function displayForm(){
+
+  }
+
+  function displayWinner(){
+
+  }
+
+  return {displayBoard, addEventListeners, endMatch};
+})();
+
+//Player Factory Function
+
+function Player(){
+  let name = '';
+  let sign = '';
+  return {name, sign}
+}
